8a361286bacc5152bb821aadd9e5137b06dd7f85
vieboo <vieboo@hotmail.com>
2025-10-29 15:25:27 +0800

    test

--- src/main/java/com/github/novicezk/midjourney/controller/TestController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.github.novicezk.midjourney.controller;
 
 import cn.hutool.core.comparator.CompareUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.github.novicezk.midjourney.VeoProperties;
 import com.github.novicezk.midjourney.support.Task;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +23,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestController {
 
-    @Resource
+    @Autowired
     private VeoProperties veoProperties;
 
     @ApiOperation(value = "查询所有任务")
     @GetMapping("/config")
-    public VeoProperties list() {
+    public VeoProperties config() {
         return veoProperties;
     }
 
